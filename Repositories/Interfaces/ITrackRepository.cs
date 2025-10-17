12abbb1730346bfab2a8d159ebfe1313e4803aad
AxelSchubert <axel.schubert@chasacademy.se>
2025-10-17 10:59:19 +0200

    Added track DTOs and repository

---
+++ Repositories/Interfaces/ITrackRepository.cs
@@ -0,0 +1,14 @@
+﻿using Hyper_Radio_API.Models;
+
+namespace Hyper_Radio_API.Repositories.Interfaces
+{
+    public interface ITrackRepository
+    {
+        Task<IEnumerable<Track>> GetAllTracksAsync();
+        Task<Track?> GetTrackByIdAsync(int id);
+        void CreateTrack(Track track);
+        void UpdateTrack(Track track);
+        void DeleteTrack(Track track);
+        Task<int> SaveChangesAsync();
+    }
+}
