10b18554135ca8a5598a992ac7f8ffb0def20254
AxelSchubert <axel.schubert@chasacademy.se>
2025-10-21 16:23:36 +0200

    Reverted prior accidental changes

---
+++ Services/ShowTrackServices/IShowTrackService.cs
@@ -0,0 +1,21 @@
+﻿using Hyper_Radio_API.DTOs;
+
+namespace Hyper_Radio_API.Services.ShowTrackServices;
+
+public interface IShowTrackService
+{
+    //GET ALL SHOWTRACKS
+    public Task<IEnumerable<ReadShowTrackDTO>> GetAllShowTracksAsync();
+
+    //GET SHOWTRACK BY ID
+    public Task<ReadShowTrackDTO> GetShowTrackByIdAsync(int showDTOId);
+
+    //CREATE SHOWTRACK
+    public Task<int> CreateShowTrackAsync(CreateShowTrackDTO showTrackDTO);
+
+    //DELETE SHOWTRACK
+    public Task<bool> DeleteShowTrackAsync(int showTrackIdDTO);
+
+    //UPDATE SHOWTRACK
+    public Task<bool> UpdateShowTrackAsync(CreateShowTrackDTO showTrackDTO);
+}
