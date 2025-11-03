f21c51ed721963c51252b3077c842e46bd55cf27
joshuauaua <joshjoelng@gmail.com>
2025-11-03 09:39:19 +0100

    Test

---
+++ Migrations/20251031141517_RemoveBitrateFromShowTracks1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hyper_Radio_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBitrateFromShowTracks1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20251031143330_DropBitrateFromShowTracks.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hyper_Radio_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropBitrateFromShowTracks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bitrate",
+                table: "ShowTracks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bitrate",
+                table: "ShowTracks",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
